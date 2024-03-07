ee415e40a071d5210062761a94a3302fe4bc1ac3
Shivam Pawar <147264094+shivampawar183@users.noreply.github.com>
2024-03-07 21:24:03 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,17 @@
 # app.py
-# This is a test commit
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+# Python3 program to add two numbers
+num1 = 15
+num2 = 12
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
